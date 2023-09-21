ad2fe02eafcedea7a5421f65ba33d13b115c9439
Chimaobi Chinaka <Chimaobi01@gmail.com>
2023-09-21 23:40:05 +0100

    add nextauthurl

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -3,28 +3,32 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 
 const authOption = {
-    providers:[
+
+    providers: [
         CredentialsProvider({
-            type: 'credentials',
+            id: "credentials",
+            name: "Credentials",
+            type: "credentials",
             credentials: {
-                email: { label: 'email', type: 'email', placeholder: 'email'},
-                password: { label: 'password', type: 'password'}
+                email: { label: 'email', type: 'email', placeholder: 'email' },
+                password: { label: 'password', type: 'password' }
             },
-            authorize(credentials){
-                const {email , password } = credentials
-                if(email === "user@example.com" && password === "1Password"){
-                    return {id: 1, name: "user", email: "user@example.com"}
+            authorize(credentials) {
+                const { email, password } = credentials
+                if (email === "user@example.com" && password === "1Password") {
+                    return { id: 1, name: "user", email: "user@example.com" }
                 }
                 return null
-                
+
             }
         })
     ],
+    
     secret: process.env.SECRET,
-  session: {
-    strategy: 'jwt',
-    maxAge: 30 * 24 * 60 * 60, // 30 Days
-  },
+    session: {
+        strategy: 'jwt',
+        maxAge: 30 * 24 * 60 * 60, // 30 Days
+    },
 }
 
 export default NextAuth(authOption)
